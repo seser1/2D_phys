fc0477ae632b6d7808209160994f426b61ec6244
seseri <***@***>
2017-08-30 22:44:04 +0900

    first commit

---
+++ 2D_physics/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace _2D_physics
+{
+    public partial class Form1 : Form
+    {
+        private static readonly int[] screen_size = new int[2] { 600, 600 };
+
+        private Manager manager = new Manager();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.Clear(Color.White);
+
+            PointF[] points = { new PointF(10, 10), new PointF(20, 10), new PointF(20, 20), new PointF(10, 20) };
+
+            g.FillPolygon(Brushes.Black, points);
+
+        }
+    }
+}
---
+++ 2D_physics/Graph.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace _2D_physics
+{
+    class Graph
+    {
+        private List<PointF> locPoints;
+        private List<PointF> pubPoints;
+        private PointF pubCenter;
+
+        public Graph()
+        {
+            locPoints = new List<PointF>();
+            //図形情報登録
+            //どこからデータ取ってくるかは考える必要あり
+            locPoints.Add(new PointF(0, 0));
+            locPoints.Add(new PointF(20, 0));
+            locPoints.Add(new PointF(20, 20));
+            locPoints.Add(new PointF(0, 20));
+
+            pubPoints = new List<PointF>();
+
+            pubCenter = new PointF(20, 20);
+        }
+
+
+        public PointF[] GetPoint()
+        {
+            /*
+            List<PointF> pubPoints = new List<PointF>();
+            locPoints.ForEach(locPoint => pubPoints.Add(locPoint));
+            */
+
+            return locPoints.ToArray();
+        }
+    }
+}
